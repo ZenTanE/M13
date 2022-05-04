074fe3ee162139b6872489fa1fbd301ce30fa407
avillarroelilg <avillarroel@ilg.cat>
2022-05-04 20:32:12 +0200

    se añaden pantallas

--- app/src/main/java/com/example/wiam2_m13/MainActivity.java
+++ app/src/main/java/com/example/wiam2_m13/MainActivity.java
@@ -8,17 +8,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button button;
+Button button,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button= findViewById(R.id.button);
+        button2= findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),home.class));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Localinfo.class));
             }
         });
     }
