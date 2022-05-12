f3ec5755a76146818943b1c40514a1fdf089bc37
avillarroelilg <avillarroel@ilg.cat>
2022-05-12 21:03:34 +0200

    objeto plato actualizadov2 y intents con los put ext. añadidos.

--- app/src/main/java/com/example/wiam2_m13/Plato.java
+++ app/src/main/java/com/example/wiam2_m13/Plato.java
@@ -4,6 +4,9 @@ public class Plato {
     String nombre;
     float rating;
     int imageUrl;
+    float price;
+    String description;
+    String opiniones;
 
     public Plato(String nombre, int imageUrl,float rating,String type) {
         this.nombre = nombre;
